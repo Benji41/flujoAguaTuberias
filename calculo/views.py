862ebde09ed643db0186dbc3e5960571dc28b0c6
Benji41 <noereynoso.business@gmail.com>
2021-11-25 00:00:01 -0700

    index html

--- calculo/views.py
+++ calculo/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello mundo! beshito pa mi baby :**")
+    return render(request,'index.html')
